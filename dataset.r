e05c38dd01b15b03ea2ed208da5e1041e0af35c7
jorditorres1098 <jorditorres1098@gmail.com>
2025-09-09 16:08:44 +0200

    some addtions

---
+++ dataset.r
@@ -0,0 +1,9 @@
+
+###This is just to simulate the data and see if seeting a seed
+
+##Define the variables first.
+n=1000
+set.seed(1234)
+x=runif(n, min=-sqrt(3), max=sqrt(3))
+epsilon= runif(n, min=-0.5, max=0.5)
+theta=2
