f00af74da2e90703769d56445d04055d3ca6301f
jorditorres1098 <jorditorres1098@gmail.com>
2025-10-19 15:51:36 +0200

    advancement

--- policy_effects/homework2.r
+++ policy_effects/homework2.r
@@ -15,7 +15,7 @@ relative_path <- "/Users/jorditorresvallverdu/Library/Mobile Documents/com~apple
 main <- read_dta(file.path(relative_path, "RD_sample.dta"))
 
 df <- as.data.frame(haven::as_factor(main))
-View(df[1:100, ])
+#View(df[1:100, ])
 
 
 #pre data-cleaning
@@ -26,8 +26,8 @@ main <- main |>
     mutate(
     time= time-120,
     population= population-500, 
-    D_pop= population<=0 ,
-    D_time= time<=0) 
+    D_pop= as.integer(population<=0) ,
+    D_time= as.integer(time>=0)) #compute the two treatment variables 
    
 
 #Note: there are some extreme values for some running variables should we consider that in the cleaning process???
@@ -41,18 +41,20 @@ R_list <- list(
   time = main$time
 )
 
+#define vector of outcomes
 Y_list <- list(
   wage = main$wage,
   score = main$score
 )
+
+#Define vector of treatment dummies
+D_list <- list(
+  population = main$D_pop,
+  time       = main$D_time
+)
+
 #define covariates to test balance
 X <- as.matrix(main[ , c( "gender", "univ", "exp_publica_c", "exp_privada_c")])
-
-
-#Define treatment
-
-
-
 
 ################
 #Some descriptives, graph analysis 
@@ -73,7 +75,7 @@ plot_data %>%
             .groups = "drop") %>%
   ggplot(aes(x = pop_mid, y = wage)) +
   geom_point(color = "darkred") +
-  geom_smooth(method = "loess", se = FALSE, color = "blue") +
+  geom_smooth(method = "loess", se = TRUE, color = "blue") +
   geom_vline(xintercept = 0, linetype = "dashed") +
   labs(title = "RD Plot: Wage vs Population (School-level, binned)")
 
@@ -86,103 +88,84 @@ plot_data %>%
             .groups = "drop") %>%
   ggplot(aes(x = pop_mid, y = score)) +
   geom_point(color = "darkred") +
-  geom_smooth(method = "loess", se = FALSE, color = "blue") +
+  geom_smooth(method = "loess", se = TRUE, color = "blue") +
   geom_vline(xintercept = 0, linetype = "dashed") +
   labs(title = "RD Plot: Score vs Population (School-level, binned)")
 
-plot_data %>%
-  filter(time >= -30 & time <= 30) %>%
-  mutate(pop_bin = cut(population, breaks = seq(-500, 500, by = 50))) %>%
-  group_by(pop_bin) %>%
-  summarise(wage = mean(wage, na.rm = TRUE),
-            pop_mid = mean(population, na.rm = TRUE),
-            .groups = "drop") %>%
-  ggplot(aes(x = pop_mid, y = wage)) +
-  geom_point(color = "darkred") +
-  geom_smooth(method = "loess", se = FALSE, color = "blue") +
-  geom_vline(xintercept = 0, linetype = "dashed") +
-  labs(title = "RD Plot: Wage vs Population (School-level, binned)")
 
+
+plot_data %>%
+  filter(time >= -89 & time <= 89) %>%
+  mutate(time_bin = cut(time, breaks = seq(-89, 89, by = 5))) %>%
+  group_by(time_bin) %>%
+  summarise(wage = mean(wage, na.rm = TRUE),
+            time_bin = mean(time, na.rm = TRUE),
+            .groups = "drop") %>%
+  ggplot(aes(x = time_bin, y = wage)) +
+  geom_point(color = "darkred") +
+  geom_smooth(method = "loess", se = TRUE, color = "blue") +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  labs(title = "RD Plot: Wage vs Time (School-level, binned)")
+
+
+plot_data %>%
+  filter(time >= -89 & time <= 89) %>%
+  mutate(time_bin = cut(time, breaks = seq(-89, 89, by = 5))) %>%
+  group_by(time_bin) %>%
+  summarise(score = mean(score, na.rm = TRUE),
+            time_bin = mean(time, na.rm = TRUE),
+            .groups = "drop") %>%
+  ggplot(aes(x = time_bin, y = score)) +
+  geom_point(color = "darkred") +
+  geom_smooth(method = "loess", se = TRUE, color = "blue") +
+  geom_vline(xintercept = 0, linetype = "dashed") +
+  labs(title = "RD Plot: score vs Time (School-level, binned)")
 
 
 
 #use program to compute the windows, but this is inefficient, cause this is an input of the previous command too. 
-window <- lapply(R_list, function(R){
-    rdwinselect(
-  R = R,
-  X = X,
-  cutoff = 0,       
-  level = 0.15,
-  statistic = "ksmirnov", 
-  reps = 1000,
-  seed = 123,
-  wmasspoints = TRUE, 
-  plot=TRUE
-) }
-)
 
-#Here I need to use the bounds in the first derivation to compute the difference in means of the main variables of interest. Why it asks this difference?
+run_all <- function(Y, R, D, X, type) {
+  # 1. Window selection
+  win <- rdwinselect(R=R, X=X, cutoff=0, statistic=type, level=0.15, reps=1000, plot = TRUE)
+    wl <- win$window[1]
+    wr <- win$window[2]
 
+  # 2. Randinf
+  ri <- rdrandinf(Y=Y, R=R, wl=wl, wr=wr, cutoff=0, fuzzy=D, statistic=type, reps=1000)
 
-##Basically the issue is that we reject the hypothesis of balance very early on, so the bandwidth is what it is...
+  # 3. Sensitivity
+  rs <- rdsensitivity(Y=Y, R=R, cutoff=0, wlist = c(2,3,4,5,6), fuzzy=D, statistic=type, reps=1000)
 
-
-#1.2.
-ranunif <- function(Y,R, type){
-        rdrandinf(
-    Y=Y,
-    R=R,
-    cutoff = 0,       
-    level = 0.15,
-    statistic = type, 
-    reps = 1000,
-    seed = 123,
-    wmasspoints = TRUE,
-    plot=TRUE
-    )
+  list(window=win, inference=ri, sensitivity=rs)
 }
+
+#rdbounds not supported, does not seem to run that well. 
+
 
 grid <- expand.grid(R=names(R_list), Y=names(Y_list))
 
-output <- apply(grid, 1, function(row){
-    Rname <- row["R"]
-    Yname <- row["Y"]
-
-    cat("iteration" ,Rname, Yname)
-
-    ranunif(Y=Y_list[[Yname]],
-    R=R_list[[Rname]], 
-    type="ksmirnov")
+output <- apply(grid, 1, function(row) {
+  Rn <- row["R"]
+  Yn <- row["Y"]
+  cat("iteration:", Rn, Yn, "\n")
+  run_all(Y_list[[Yn]], R_list[[Rn]], D_list[[Rn]], X, type="diffmeans")
 })
 
-#why I don't have good graphs here? work on this too, get the correct graphs. Good formated. 
 
-##Fins aquí tinc una noció clara del que he de fer
-
-rdbounds <- function(Y,R, type){
-  rdrbounds(
-  Y=Y,
-  R=R,
-  statistic = type,
-  evalat = "cutoff",
-  kernel = "uniform",
-  reps = 1000,
-  seed = 123
-  )
-}
+##To review at some other time; ask for the code...
 
 
-output2 <- apply(grid, 1, function(row){
-    Rname <- row["R"]
-    Yname <- row["Y"]
 
-    cat("iteration" ,Rname, Yname)
+##Exercise 2.
 
-    rdbounds(Y=Y_list[[Yname]],
-    R=R_list[[Rname]], 
-    type="ksmirnov")
-})
 
-#don't know why this is not working...Maybe not applying well the command
+
+
+
+
+
+
+
 
 ############################################## EOF
